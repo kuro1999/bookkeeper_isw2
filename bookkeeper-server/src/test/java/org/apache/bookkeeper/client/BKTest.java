1a9b678965cfc689fdfc7a11a1eb1ad9d58bafa6
kuro1999 <edoardo.marchionni99@gmail.com>
2025-06-06 17:52:24 +0200

    ready to test

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/BKTest.java
@@ -0,0 +1,13 @@
+package org.apache.bookkeeper.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BKTest {
+    @Test
+    public void testBKTest() throws Exception {
+        BookKeeper bk = new BookKeeper();
+        assertTrue(true);
+    }
+}
